574a9608fffc386d1fbff9cc35c1c69aeae03505
rogeriocsc <rogeriocurtiogithub@gmail.com>
2024-04-13 17:26:38 -0300

    Atualização login

--- login_cfbcursos.js
+++ login_cfbcursos.js
@@ -128,13 +128,14 @@ class Login{
                 this.nomelogado=res.nome;
                 this.acessologado=res.acesso;
                 this.callback_ok();
-                this.fechar();
+                
             }else{
                 this.logado=false;
                 this.matlogado=null;
                 this.nomelogado=null;
                 this.acessologado=null;
                 this.callback_naook();
+                this.fechar();
             }
         })
     }
